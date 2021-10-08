4099c9043fd924bf5f932f44f89d89ee2907b39a
Amutha Karunamoorthy <amutha.amk@gmail.com>
2021-10-08 09:36:03 -0500

    Stock and weather pages

--- src/wwwroot/js/monitoring.js
+++ src/wwwroot/js/monitoring.js
@@ -2,7 +2,7 @@
 // Client side code used on Monitor.cshtml page
 //
 
-const DATA_INTERVAL = 3000;
+const DATA_INTERVAL = 10000;
 var wsChart, cpuCHart;
 
 // START HERE - Called on window load by Monitor.cshtml
@@ -14,7 +14,7 @@ function startMonitoring() {
       data: {
           labels: [],
           datasets: [{
-            label: 'Working Set (MBytes)',
+            label: 'Low',
             data: [],
             borderColor: 'rgba(0, 156, 220, 1.0)',
             backgroundColor: 'rgba(0, 156, 220, 0.4)',
@@ -32,7 +32,7 @@ function startMonitoring() {
       data: {
           labels: [],
           datasets: [{
-            label: 'Processor Load (%)',
+            label: 'high',
             data: [],
             borderColor: 'rgba(19, 185, 85, 1.0)',
             backgroundColor: 'rgba(19, 185, 85, 0.4)',
@@ -72,20 +72,36 @@ function addData(chart, label, data) {
 // Call API to get data
 //
 function getData() {
-  fetch('/api/monitoringdata')
+  fetch('https://api.polygon.io/v1/open-close/AAPL/2020-10-14?adjusted=true&apiKey=QVqGLujlpOSNtTp5nokxwAYVw0aiDAoh', {
+    "method": "GET"
+  })
+ // fetch('/api/monitoringdata')
   .then((response) => {
     // fetch handles errors strangely, we need to trap non-200 codes here
     if (!response.ok) {
       throw Error(response.statusText + " " + response.status);
     }    
-    return response.json();
-  })
-  .then((data) => {
-    let d = new Date();
+    console.log(response);
+    return response.json().then(function(json) {
+      console.log(json);
+      let d = new Date();
 
     // Add results to the two charts
-    addData(wsChart, d.getHours()+":"+d.getMinutes()+":"+d.getSeconds(), data.workingSet / (1024*1024));          
-    addData(cpuChart, d.getHours()+":"+d.getMinutes()+":"+d.getSeconds(), data.cpuPercentage);          
+    addData(wsChart, d.getHours()+":"+d.getMinutes()+":"+d.getSeconds(), json.low);          
+    addData(cpuChart, d.getHours()+":"+d.getMinutes()+":"+d.getSeconds(), json.high); 
+    let wList = document.getElementById('weather-list')
+    wList.innerHTML = "";
+      wList.innerHTML += `<li>Symbol: &nbsp; ${json.symbol}</li>`
+      wList.innerHTML += `<li>Low: &nbsp; ${json.low}</li>`
+      wList.innerHTML += `<li>High: &nbsp; ${json.high}</li>` 
+    });
+  })
+  .then((data) => {
+   // let d = new Date();
+
+    // Add results to the two charts
+   // addData(wsChart, d.getHours()+":"+d.getMinutes()+":"+d.getSeconds(), data.workingSet / (1024*1024));          
+   // addData(cpuChart, d.getHours()+":"+d.getMinutes()+":"+d.getSeconds(), data.cpuPercentage);          
   })
   .catch(err => {
     console.log(err)
--- src/wwwroot/js/weather.js
+++ src/wwwroot/js/weather.js
@@ -4,70 +4,49 @@
 
 // DIV to inform users of problems
 let errorBox = document.getElementById('errorBox');
-
-// Try to use the browser geolocation API
-// This only works on HTTPS (or localhost) and requires users permission
-if (navigator.geolocation) {
-  errorBox.classList.add('hide');
-  navigator.geolocation.getCurrentPosition(getWeather, (err) => {  
-    let errMessage = err.message;
-    if(err.message.startsWith("Only secure origins are allowed")) {
-      errMessage = "getCurrentPosition API only works on secure (HTTPS) domains";
-    } 
-    errorBox.classList.add('show');
-    errorBox.innerHTML = errMessage + ". Will fall back to showing weather for London";
-    getWeather({ coords: { latitude: 51.40329, longitude: 0.05619 }})
-  });
-} else {
-  errorBox.classList.add('show');
-  errorBox.innerHTML = "Geolocation is not supported by this browser. Maybe it's time to upgrade!";
-  getWeather({ coords: { latitude: 51.40329, longitude: 0.05619 }})
-}
+getWeather();
 
 // Get the weather data from the API
 // Of a given position (see https://developer.mozilla.org/en-US/docs/Web/API/GeolocationPosition)
-function getWeather(pos) {
+function getWeather() {
 
   let spinner = document.getElementById('spinner');
-  let lat = pos.coords.latitude;    //51.40329 
-  let long = pos.coords.longitude;  //0.05619 
+  //let lat = pos.coords.latitude;    //51.40329 
+  //let long = pos.coords.longitude;  //0.05619 
+  let zipcode = 75093;
+  let APIkey = '676398400456080ce88d9882d01d59b2';
 
   // Call API with fetch
-  fetch(`/api/weather/${lat}/${long}`)
+  //fetch(`/api/weather/${lat}/${long}`)
+  //fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=${APIkey}`)//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+  fetch('https://api.openweathermap.org/data/2.5/weather?zip=75093,us&appid=676398400456080ce88d9882d01d59b2', {
+  "method": "GET"
+})
   .then((response) => {
     // fetch handles errors strangely, we need to trap non-200 codes here
     if (!response.ok) {
       throw Error(response.statusText + " " + response.status);
     }
+    console.log(response);
     // Convert JSON results
-    return response.json();
+    //console.log(response.json());
+    return response.json().then(function(json) {
+      console.log(json);
+      let wList = document.getElementById('weather-list')
+      wList.innerHTML += `<li>City: &nbsp; ${json.name}</li>`
+      wList.innerHTML += `<li>Country: &nbsp; ${json.sys.country}</li>`
+      for (const result of json.weather) {
+       
+        wList.innerHTML += `<li>The weather currently is: &nbsp; ${result.description}</li>`
+      }
+     
+    });
   })
   .then((weather) => {
     // Got some real data, so...
     spinner.parentNode.removeChild(spinner);
-    
-    // Set & display skycon icon
-    var skycons = new Skycons({ "color": "#3498db" });
-    skycons.add("weather_icon", weather.currently.icon);
-    skycons.play();
-
-    // Get weather details and show in HTML list
-    let wList = document.getElementById('weather-list')
-    wList.innerHTML += `<li>The weather currently is: &nbsp; ${weather.currently.summary}</li>`
-    wList.innerHTML += `<li>The temperature is: &nbsp; ${weather.currently.temperature}°C</li>`
-    wList.innerHTML += `<li>The temperature feels like: &nbsp; ${weather.currently.apparentTemperature}°C</li>`
-    wList.innerHTML += `<li>Chance of rain is: &nbsp; ${weather.currently.precipProbability}%</li>`    
-    wList.innerHTML += `<li>The humidity is: &nbsp; ${weather.currently.humidity}%</li>`    
-    wList.innerHTML += `<li>Wind speed: &nbsp; ${weather.currently.windSpeed}%</li>`    
-    wList.innerHTML += `<li>UV index: &nbsp; ${weather.currently.uvIndex}%</li>`    
-    wList.innerHTML += `<li>Forecast hour: &nbsp; ${weather.minutely.summary}</li>`    
-    wList.innerHTML += `<li>Forecast day: &nbsp; ${weather.hourly.summary}</li>`    
-    wList.innerHTML += `<li>Forecast week: &nbsp; ${weather.daily.summary}</li>`    
   })
   .catch(err => {
-    // Error trapping
-    spinner.parentNode.removeChild(spinner);
-    error.classList.add('show');
-    error.innerHTML = "Weather API Error - " + err;
+    console.log(err);
   });
 }
