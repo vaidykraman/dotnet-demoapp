9ecaf8877f473b15837776fc5a14647ad21e18e8
Amutha Karunamoorthy <amutha.amk@gmail.com>
2021-10-08 10:45:59 -0500

    Index and weather - Add additional cities changes

--- src/wwwroot/js/weather.js
+++ src/wwwroot/js/weather.js
@@ -11,14 +11,69 @@ getWeather();
 function getWeather() {
 
   let spinner = document.getElementById('spinner');
-  //let lat = pos.coords.latitude;    //51.40329 
-  //let long = pos.coords.longitude;  //0.05619 
-  let zipcode = 75093;
-  let APIkey = '676398400456080ce88d9882d01d59b2';
 
+  //Weather1
   // Call API with fetch
-  //fetch(`/api/weather/${lat}/${long}`)
-  //fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=${APIkey}`)//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+  fetch('https://api.openweathermap.org/data/2.5/weather?zip=30301,us&appid=676398400456080ce88d9882d01d59b2', {
+  "method": "GET"
+})
+  .then((response) => {
+    // fetch handles errors strangely, we need to trap non-200 codes here
+    if (!response.ok) {
+      throw Error(response.statusText + " " + response.status);
+    }
+    console.log(response);
+    // Convert JSON results
+    return response.json().then(function(json) {
+      console.log(json);
+      let wList = document.getElementById('weather-list1')
+      wList.innerHTML += `<li>City: &nbsp; ${json.name}</li>`
+      wList.innerHTML += `<li>Country: &nbsp; ${json.sys.country}</li>`
+      for (const result of json.weather) {
+       
+        wList.innerHTML += `<li>The weather currently is: &nbsp; ${result.description}</li>`
+      }
+     
+    });
+  })
+  .then((weather) => {
+    // Got some real data, so...
+    spinner.parentNode.removeChild(spinner);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+//Weather2
+  fetch('https://api.openweathermap.org/data/2.5/weather?zip=10001,us&appid=676398400456080ce88d9882d01d59b2', {
+  "method": "GET"
+})
+  .then((response) => {
+    // fetch handles errors strangely, we need to trap non-200 codes here
+    if (!response.ok) {
+      throw Error(response.statusText + " " + response.status);
+    }
+    console.log(response);
+    // Convert JSON results
+    return response.json().then(function(json) {
+      console.log(json);
+      let wList = document.getElementById('weather-list2')
+      wList.innerHTML += `<li>City: &nbsp; ${json.name}</li>`
+      wList.innerHTML += `<li>Country: &nbsp; ${json.sys.country}</li>`
+      for (const result of json.weather) {
+       
+        wList.innerHTML += `<li>The weather currently is: &nbsp; ${result.description}</li>`
+      }
+     
+    });
+  })
+  .then((weather) => {
+    // Got some real data, so...
+    spinner.parentNode.removeChild(spinner);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
   fetch('https://api.openweathermap.org/data/2.5/weather?zip=75093,us&appid=676398400456080ce88d9882d01d59b2', {
   "method": "GET"
 })
@@ -29,10 +84,39 @@ function getWeather() {
     }
     console.log(response);
     // Convert JSON results
-    //console.log(response.json());
     return response.json().then(function(json) {
       console.log(json);
-      let wList = document.getElementById('weather-list')
+      let wList = document.getElementById('weather-list3')
+      wList.innerHTML += `<li>City: &nbsp; ${json.name}</li>`
+      wList.innerHTML += `<li>Country: &nbsp; ${json.sys.country}</li>`
+      for (const result of json.weather) {
+       
+        wList.innerHTML += `<li>The weather currently is: &nbsp; ${result.description}</li>`
+      }
+     
+    });
+  })
+  .then((weather) => {
+    // Got some real data, so...
+    spinner.parentNode.removeChild(spinner);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+  fetch('https://api.openweathermap.org/data/2.5/weather?zip=60606,us&appid=676398400456080ce88d9882d01d59b2', {
+  "method": "GET"
+})
+  .then((response) => {
+    // fetch handles errors strangely, we need to trap non-200 codes here
+    if (!response.ok) {
+      throw Error(response.statusText + " " + response.status);
+    }
+    console.log(response);
+    // Convert JSON results
+    return response.json().then(function(json) {
+      console.log(json);
+      let wList = document.getElementById('weather-list4')
       wList.innerHTML += `<li>City: &nbsp; ${json.name}</li>`
       wList.innerHTML += `<li>Country: &nbsp; ${json.sys.country}</li>`
       for (const result of json.weather) {
